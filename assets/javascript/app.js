6f3db38059e5bd139ed4f5a7f88da4fa6ea3512a
CKayV11 <11303975+ckayv11@users.noreply.github.com>
2019-03-31 19:40:36 -0500

    Made edits to formatting

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,23 +15,23 @@ var dogs = ["German Shepherd", "Golden Retriever", "Black Lab", "Pitbull Dog"];
     }).then(function(response) {
       
       for (var i = 0; i < response.data.length; i++) {
-      // Dynamically creating a div to hold the dog type, setting variables to retrieve images URLs, creating element to hold image and assign attributes
-      var dogDiv = $("<div>");
-      dogDiv.addClass("col-md-4 responsive");
-      var stillImageURL = response.data[i].images.fixed_height_still.url;
-      var animateImageURL = response.data[i].images.fixed_height.url;
-      var imgURL = $("<img>").attr("src", stillImageURL);
-      imgURL.attr("data-still", stillImageURL);
-      imgURL.attr("data-animate", animateImageURL);
-      imgURL.attr("data-state", "still");
-      imgURL.addClass("gif");
-      // Storing the rating data & creating an element to have the rataing displayed
-      var ratingData = response.data[i].rating;
-      var ratingDisplay = $("<p>").text("Rating: " + ratingData);
-      // Append image and ratings & then prepending the dogDiv to the HTML page in the "#dog-view" div
-      dogDiv.append(imgURL, ratingDisplay);
-      $("#dog-view").prepend(dogDiv);
-      $("#userMessage").show();
+          // Dynamically creating a div to hold the dog type, setting variables to retrieve images URLs, creating element to hold image and assign attributes
+          var dogDiv = $("<div>");
+          dogDiv.addClass("col-md-4 responsive");
+          var stillImageURL = response.data[i].images.fixed_height_still.url;
+          var animateImageURL = response.data[i].images.fixed_height.url;
+          var imgURL = $("<img>").attr("src", stillImageURL);
+          imgURL.attr("data-still", stillImageURL);
+          imgURL.attr("data-animate", animateImageURL);
+          imgURL.attr("data-state", "still");
+          imgURL.addClass("gif");
+          // Storing the rating data & creating an element to have the rataing displayed
+          var ratingData = response.data[i].rating;
+          var ratingDisplay = $("<p>").text("Rating: " + ratingData);
+          // Append image and ratings & then prepending the dogDiv to the HTML page in the "#dog-view" div
+          dogDiv.append(imgURL, ratingDisplay);
+          $("#dog-view").prepend(dogDiv);
+          $("#userMessage").show();
       };
     });
   }; //End of displayDogs function
@@ -40,35 +40,35 @@ var dogs = ["German Shepherd", "Golden Retriever", "Black Lab", "Pitbull Dog"];
   function renderButtons() {
     $("#buttons-view").empty();
     for (var i = 0; i < dogs.length; i++) {
-      // Dynamicaly generating buttons for each dog in the array and adding classes/attributes to buttons
-      var btn = $("<button>");
-      btn.addClass("dog-btn btn btn-info m-1");
-      btn.attr("data-dog", dogs[i]);
-      btn.text(dogs[i]);
-      // Adding the button to the buttons-view div
-      $("#buttons-view").append(btn);
+        // Dynamicaly generating buttons for each dog in the array and adding classes/attributes to buttons
+        var btn = $("<button>");
+        btn.addClass("dog-btn btn btn-info m-1");
+        btn.attr("data-dog", dogs[i]);
+        btn.text(dogs[i]);
+        // Adding the button to the buttons-view div
+        $("#buttons-view").append(btn);
     };
   };
 
   // Function handles events where submit button is clicked
   $("#add-dog").on("click", function(event) {
-    event.preventDefault();
-    // Grab the input from the textbox & add to the array with push
-    var dog = $("#form-input").val().trim();
-    dogs.push(dog);
-    // Call renderButtons which handles the processing of our dog array
-    renderButtons();
+      event.preventDefault();
+      // Grab the input from the textbox & add to the array with push
+      var dog = $("#form-input").val().trim();
+      dogs.push(dog);
+      // Call renderButtons which handles the processing of our dog array
+      renderButtons();
   });
 
   // Function to pause and start gifs
   function playPauseGifs() {
     var state = $(this).attr("data-state");
     if (state === "still") {
-      $(this).attr("src", $(this).attr("data-animate"));
-      $(this).attr("data-state", "animate");
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
     } else {
-      $(this).attr("src", $(this).attr("data-still"));
-      $(this).attr("data-state", "still");
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
     };
   };
     
